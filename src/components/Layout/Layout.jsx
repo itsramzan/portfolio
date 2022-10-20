a721bb44aa5e3185af5e8d2620953e445095e5f0
itsrimon <its.ramzan.always@gmail.com>
2022-10-21 00:15:07 +0600

    Banner & About section done

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "../Shared/Navbar";
-import Footer from "../Shared/Footer";
 
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen w-full grid grid-rows-[auto,1fr,auto]">
       <Navbar />
       {children}
-      {/* <Footer /> */}
     </div>
   );
 };
